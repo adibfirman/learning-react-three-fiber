698a8360a22f5c8c9a59868121638693a1883767
qlue <dib.firman@gmail.com>
2020-06-06 13:21:36 +0700

    initial box with

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
+import "./style.css"
 import React from "react"
+import { Canvas } from "react-three-fiber"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Canvas>
+      <mesh>
+        <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+        <meshBasicMaterial attach="material" color="red" />
+      </mesh>
+    </Canvas>
+  )
 }
